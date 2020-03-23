5a135d1f79126cd43ccb3437bbc351b15c584866
Travis Mendoza <travmendoz@gmail.com>
2020-03-23 15:36:59 -0600

    Initial Commit

---
+++ SpokenWord Persistence Edit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpokenWord Persistence Edit
+//
+//  Created by Travis's Macbook on 23/03/20.
+//  Copyright © 2020 Travis Mendoza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SpokenWord Persistence EditTests/SpokenWord_Persistence_EditTests.swift
@@ -0,0 +1,34 @@
+//
+//  SpokenWord_Persistence_EditTests.swift
+//  SpokenWord Persistence EditTests
+//
+//  Created by Travis's Macbook on 23/03/20.
+//  Copyright © 2020 Travis Mendoza. All rights reserved.
+//
+
+import XCTest
+@testable import SpokenWord_Persistence_Edit
+
+class SpokenWord_Persistence_EditTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
